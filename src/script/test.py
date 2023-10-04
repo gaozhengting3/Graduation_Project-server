7c2d30d0f94bd7291526d69b923fdda0924c0878
snill901123 <snill901123@gmail.com>
2023-10-04 19:30:09 +0800

    1

--- src/script/test.py
+++ src/script/test.py
@@ -1,25 +1,11 @@
-from faceRecognize import faceRecognize
+import os
+from os import path
+import random
+import time
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import os
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/')
-def index():
-    return 'hello'
-
-@app.route('/test', methods=['POST'])
-def test():
-    print("=========")
-    data = request.json
-    filename = data["filename"]
-    path = os.getcwd()
-    print(path)
-    after = os.path.join(path, filename)
-    print(after)
-    print("=========")
-    return ""
-if __name__ == '__main__':
-    app.run(port=8001, debug=True)
+import mylib
+path = os.getcwd() #　C:\workspace\server\src\script
+print(path)
+print(os.path.join([path,"abc","cde"]))
