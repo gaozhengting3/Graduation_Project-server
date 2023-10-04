8c455fe8a39a468ab166cfe2304d866051e3214d
snill901123 <snill901123@gmail.com>
2023-10-04 14:59:09 +0800

    Update test.py

--- src/script/test.py
+++ src/script/test.py
@@ -1,5 +1,25 @@
-def main():
-  print('hello')
+from faceRecognize import faceRecognize
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import os
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return 'hello'
+
+@app.route('/test', methods=['POST'])
+def test():
+    print("=========")
+    data = request.json
+    filename = data["filename"]
+    path = os.getcwd()
+    print(path)
+    after = os.path.join(path, filename)
+    print(after)
+    print("=========")
+    return ""
 if __name__ == '__main__':
-  main()
+    app.run(port=8001, debug=True)
